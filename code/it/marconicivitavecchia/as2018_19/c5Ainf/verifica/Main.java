61c9d74a08fc5b7f356a830f48fd3f3066b4f35b
DaniloFappiano <ddanilo731@gmail.com>
2018-11-17 10:37:41 +0100

    consegna compito fappiano

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -12,16 +12,37 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(500,500);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html>");
+		sb.append("<h1> SSh First Aid </h1>");
 		
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<div style=display table>");
+		sb.append("<div style=display table-row>");
+		sb.append("alias");
+		sb.append("</div>");
+		sb.append("</div>");
+		sb.append("<div style=display table-row>");
+		sb.append("raspberry");
+		sb.append("</div>");
+		sb.append("<div style=display table-row>");
+		sb.append("coreswitch");
+		sb.append("</div>");
 		
-		// TODO Convertire lo StringBuilder in String
+		
+		
+		
+		
+		
+		
+	    // TODO Convertire lo StringBuilder in String
+		String htmltext = sb.toString();
 		
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+		super.add(new JLabel(htmltext));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
